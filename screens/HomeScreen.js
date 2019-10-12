a297400282264c2925e731325e44ddf5828ce493
Jeffrey Meng <jeffkmeng@gmail.com>
2019-10-12 16:16:27 -0700

    move home to today

--- screens/HomeScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ToDoList from '../components/ToDoList';
-
-export default function App() {
-  return (<ToDoList></ToDoList>)
-}
---
+++ screens/today/TodayScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ToDoList from '../../components/ToDoList';
+
+export default function TodayScreen() {
+  return (<ToDoList></ToDoList>)
+}
+TodayScreen.navigationOptions = {
+  title: "Today",
+};
+
