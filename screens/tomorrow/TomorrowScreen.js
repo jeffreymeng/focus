22e8055d2570b8af37fcbcda751e8ca1414b6adc
richyliu <richy.liu.2002@gmail.com>
2019-10-13 09:13:06 -0700

    fix tomorrow edit todo bug

--- screens/tomorrow/TomorrowScreen.js
+++ screens/tomorrow/TomorrowScreen.js
@@ -10,29 +10,64 @@ export default function TomorrowScreen({ navigation }) {
   const [todoItems, setTodoItems] = React.useState([]);
   const userId = auth.currentUser.uid;
 
-  React.useEffect(() => {
+  React.useEffect(
+    () =>
+      db
+        .collection('users')
+        .doc(userId)
+        .collection('todos')
+        .onSnapshot(querySnapshot => {
+          let todos = [];
+          querySnapshot.forEach(doc => {
+            todos.push({
+              id: doc.id,
+              ...doc.data(),
+            });
+          });
+          setTodoItems(
+            todos.filter(
+              x => new Date(x.date).getDate() === new Date().getDate() + 1
+            )
+          );
+        }),
+    []
+  );
+
+  function onCompletionChange(id, checked) {
+    let newTodo = [];
+    setTodoItems(
+      todoItems.map(todo => {
+        if (todo.id === id) {
+          newTodo = { ...todo, checked };
+          return newTodo;
+        }
+        return todo;
+      })
+    );
     db.collection('users')
       .doc(userId)
       .collection('todos')
-      .onSnapshot(querySnapshot => {
-        let todos = [];
-        querySnapshot.forEach(doc => {
-          todos.push({
-            id: doc.id,
-            ...doc.data(),
-          });
-        });
-        setTodoItems(
-          todos.filter(
-            x => new Date(x.date).getDate() === new Date().getDate() + 1
-          )
-        );
-      });
-  }, []);
+      .doc(id)
+      .set(newTodo);
+  }
+
+  function editTodo(id, title, date, checked) {
+    navigation.navigate('EditItem', {
+      from: 'Tomorrow',
+      title,
+      date,
+      id,
+      checked,
+    });
+  }
 
   return (
     <View style={{ height: '100%' }}>
-      <ToDoList todoItems={todoItems} />
+      <ToDoList
+        todoItems={todoItems}
+        onCompletionChange={onCompletionChange}
+        onSelected={editTodo}
+      />
       <Fab
         style={{ backgroundColor: '#5067FF' }}
         position="bottomRight"
