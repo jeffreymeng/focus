eea18c545ceb6a11c7e83ff3c65449836931d221
richyliu <richy.liu.2002@gmail.com>
2019-10-12 22:05:23 -0700

    move todolist read to today screen

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -11,29 +11,9 @@ import {
 import ToDoListItem from '../components/ToDoListItem';
 import Constants from 'expo-constants';
 import TabBarIcon from './TabBarIcon';
-import { db } from '../firebase';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'Do Math Homework',
-    subtitle: 'subtitle',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Do Chem Homework',
-    subtitle: 'subtitle',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Feed the cat',
-    subtitle: 'subtitle',
-  },
-];
-
-export default function ToDoList() {
+export default function ToDoList({ todoItems }) {
   const [selected, setSelected] = React.useState(new Map());
-  const [data, setData] = React.useState([]);
 
   const onSelect = React.useCallback(id => {
     const newSelected = new Map(selected);
@@ -42,21 +22,10 @@ export default function ToDoList() {
     setSelected(newSelected);
   }, [selected]);
 
-  React.useEffect(() => {
-    db.collection('users')
-      .doc('john')
-      .get()
-      .then(doc => {
-        if (doc.exists) {
-          setData(doc.data().todo);
-        }
-      });
-  });
-
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={data}
+        data={todoItems}
         renderItem={({ item }) => (
           <ToDoListItem
             id={item.id}
--- screens/today/TodayScreen.js
+++ screens/today/TodayScreen.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ToDoList from '../../components/ToDoList';
+import { db } from '../../firebase';
+
+const TEST_DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Do Math Homework',
+    subtitle: 'subtitle',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Do Chem Homework',
+    subtitle: 'subtitle',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Feed the cat',
+    subtitle: 'subtitle',
+  },
+];
 
 export default function TodayScreen() {
-  return (<ToDoList></ToDoList>)
+  const [todoItems, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    console.log('test');
+    setTimeout(() => {
+      setData(TEST_DATA);
+    }, 100);
+    return;
+
+    db.collection('users')
+      .doc('john')
+      .get()
+      .then(doc => {
+        if (doc.exists) {
+          setData(doc.todoItems().todo);
+        }
+      });
+  });
+
+  return <ToDoList todoItems={todoItems} />;
 }
 TodayScreen.navigationOptions = {
-  title: "Today",
+  title: 'Today',
 };
